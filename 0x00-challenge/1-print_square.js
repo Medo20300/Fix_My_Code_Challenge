3336ee38aaa5b240241fd22b3bb35b8dda16602e
Medo20300 <medhatconcrete@gmail.com>
2024-02-21 13:00:57 -0800

    2030

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -7,21 +7,21 @@
 */
 
 
-if (process.argv.length <= 2) {
+if (process.argv.length <= 2) 
+{
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1)
 }
-/*
-     By removing the radix parameter(16), parseInt will default to base 10
 
-*/
-size = parseInt(process.argv[2])
+size = parseInt(process.argv[2], 10)
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let i = 0 ; i < size ; i ++) 
+{
+    for (let j = 0 ; j < size ; j ++) 
+	{
         process.stdout.write("#");
-    }
+    	}
     process.stdout.write("\n");
 }
